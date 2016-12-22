7b826b4547f5623e390127dd2e4870bdd72402f4
yuanlovejie <xiaocui19901017@163.com>
2016-12-22 10:36:17 +0800

    Add files via upload

---
+++ test.java
@@ -0,0 +1,37 @@
+package cfy.bio;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+/**
+ * Created by xxx on 2016/12/15.
+ */
+public class Server {
+
+    final static int port = 8765;
+
+    public static void main(String[] args) {
+        ServerSocket server = null;
+        try {
+             server = new ServerSocket(port);
+             System.out.println("服务端启动---");
+            //阻塞服务端 一直到客户端请求连接
+             server.accept();
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            if(server != null){
+                try {
+                    server.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            server =null;
+        }
+
+    }
+
+}
